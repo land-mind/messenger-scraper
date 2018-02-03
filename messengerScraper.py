f312cf2a45af5de4c425686de14044b59b33860f
marsalad <buggymarsala@gmail.com>
2018-02-02 23:33:25 -0500

    get all html files

--- messengerScraper.py
+++ messengerScraper.py
@@ -1,5 +1,16 @@
 from bs4 import BeautifulSoup
-import csv
+import os, csv
 
-src_folder = 'data'
-out_file = 'scraped.csv'
+srcFolder = 'data'
+outFile = 'scraped.csv'
+
+def scrapePage(fname):
+	pass
+
+def scrapeAll(folder):
+	for file in os.listdir(folder):
+	    if file.endswith('.html'):
+	        scrapePage(os.path.join(folder, file))
+
+if __name__ == "__main__":
+	print(scrapeAll('data'))
