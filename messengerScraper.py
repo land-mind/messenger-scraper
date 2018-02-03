f2a5cba811e13bd855a90309dd3b6f312d9394a5
marsalad <buggymarsala@gmail.com>
2018-02-03 00:23:31 -0500

    can extract data

--- messengerScraper.py
+++ messengerScraper.py
@@ -4,13 +4,19 @@ import os, csv
 srcFolder = 'data'
 outFile = 'scraped.csv'
 
-def scrapePage(fname):
-	pass
+def scrapePage(file):
+	soup_page = BeautifulSoup(open(file), 'html.parser')
+	thread = soup_page.find('div', attrs={'class' : 'thread'})
+	metas = thread.find_all('div', attrs={'class' : 'message_header'})
+
+	users = [meta.find('span', attrs={'class' : 'user'}, recursive=False).text for meta in metas]
+	times = [meta.find('span', attrs={'class' : 'meta'}, recursive=False).text for meta in metas]
+	msgs = [msg.text for msg in thread.find_all('p', recursive=False)]
 
 def scrapeAll(folder):
 	for file in os.listdir(folder):
 	    if file.endswith('.html'):
-	        scrapePage(os.path.join(folder, file))
+	        scrapePage(os.path.join(folder, file)):
 
 if __name__ == "__main__":
-	print(scrapeAll('data'))
+	scrapeAll('data')
