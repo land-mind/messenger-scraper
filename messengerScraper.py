a6c1f90a953892ac0e19dcc76abb68d5bd593d46
marsalad <buggymarsala@gmail.com>
2018-02-03 03:39:49 -0500

    grab name from htm file

--- messengerScraper.py
+++ messengerScraper.py
@@ -17,10 +17,18 @@ def scrapePage(file):
 def strToTime(str):
 	return datetime.strptime(str, '%A, %B %d, %Y at %I:%M%p %Z')
 
-# returns array of all messages in folder in format (time, user, text)
+# returns array of all messages in folder in format name, [(time, user, text)]
 def scrapeAll(folder):
+	name = getName(os.path.join(folder, 'index.htm'))
+
 	msgs = []
 	for file in os.listdir(folder):
 	    if file.endswith('.html'):
 	        msgs.extend(scrapePage(os.path.join(folder, file)))
-	return sorted(msgs, key=lambda x: (x[0], x[1]))
+	
+	return name, msgs
+
+# returns the name of the target user
+def getName(file):
+	soup_page = BeautifulSoup(open(file), 'html.parser')
+	return soup_page.find('div', attrs={'class' : 'contents'}).find('h1').text
