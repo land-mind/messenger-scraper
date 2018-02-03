6da0cbb08d580b54595cb9bf6685dbf6f3377448
marsalad <buggymarsala@gmail.com>
2018-02-03 01:27:00 -0500

    datetime formatting

--- messengerScraper.py
+++ messengerScraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-import os, csv
+from datetime import datetime
+import os
 
 srcFolder = 'data'
 outFile = 'scraped.csv'
@@ -9,14 +10,20 @@ def scrapePage(file):
 	thread = soup_page.find('div', attrs={'class' : 'thread'})
 	metas = thread.find_all('div', attrs={'class' : 'message_header'})
 
+	times = [strToTime(meta.find('span', attrs={'class' : 'meta'}, recursive=False).text) for meta in metas]
 	users = [meta.find('span', attrs={'class' : 'user'}, recursive=False).text for meta in metas]
-	times = [meta.find('span', attrs={'class' : 'meta'}, recursive=False).text for meta in metas]
-	msgs = [msg.text for msg in thread.find_all('p', recursive=False)]
+	texts = [text.text for text in thread.find_all('p', recursive=False)]
+	return zip(times, users, texts)
+
+def strToTime(str):
+	return datetime.strptime(str, '%A, %B %d, %Y at %I:%M%p %Z')
 
 def scrapeAll(folder):
+	msgs = []
 	for file in os.listdir(folder):
 	    if file.endswith('.html'):
-	        scrapePage(os.path.join(folder, file)):
+	        msgs.extend(scrapePage(os.path.join(folder, file)))
+
 
 if __name__ == "__main__":
 	scrapeAll('data')
