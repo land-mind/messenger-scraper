c6925712785ffb0c4ac396d161bf64059c4f1056
marsalad <buggymarsala@gmail.com>
2018-02-03 01:34:32 -0500

    finished scraper code

--- messengerScraper.py
+++ messengerScraper.py
@@ -2,9 +2,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import os
 
-srcFolder = 'data'
-outFile = 'scraped.csv'
-
+# returns array of all messages in file in format (time, user, text)
 def scrapePage(file):
 	soup_page = BeautifulSoup(open(file), 'html.parser')
 	thread = soup_page.find('div', attrs={'class' : 'thread'})
@@ -15,15 +13,14 @@ def scrapePage(file):
 	texts = [text.text for text in thread.find_all('p', recursive=False)]
 	return zip(times, users, texts)
 
+# returns datetime object of str
 def strToTime(str):
 	return datetime.strptime(str, '%A, %B %d, %Y at %I:%M%p %Z')
 
+# returns array of all messages in folder in format (time, user, text)
 def scrapeAll(folder):
 	msgs = []
 	for file in os.listdir(folder):
 	    if file.endswith('.html'):
 	        msgs.extend(scrapePage(os.path.join(folder, file)))
-
-
-if __name__ == "__main__":
-	scrapeAll('data')
+	return sorted(msgs, key=lambda x: (x[0], x[1]))
